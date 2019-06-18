b5d71c99b83245fdff595196db40ca26acf6c289
itsSiddharthGupta <36455409+itsSiddharthGupta@users.noreply.github.com>
2019-06-18 12:40:40 +0530

    Market search dialog box done

--- app/src/main/java/com/example/kesbokar/MarketListing.java
+++ app/src/main/java/com/example/kesbokar/MarketListing.java
@@ -8,7 +8,10 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
+import android.app.LoaderManager;
 import android.content.Intent;
+import android.content.Loader;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -21,13 +24,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -48,6 +58,25 @@ public class MarketListing extends AppCompatActivity implements NavigationView.O
     private DataAdapterMarket dataAdapter;
     private ArrayList<MarketIem> marketIems;
     private RequestQueue requestQueue;
+
+    private AutoCompleteTextView autoCompleteTextViewOne,autoCompleteTextViewTwo;
+    private Button btnAlertDialogSearch;
+
+    private static final int LOADER_ID_MARVAL = 3;
+    private static final int LOADER_ID_MARSUB = 4;
+    private static final int LOADER_ID_BTNSRCH = 5;
+
+    private LoaderManager.LoaderCallbacks<ArrayList<String>> marketSearch;
+    private LoaderManager.LoaderCallbacks<ArrayList<StateAndSuburb>> marketSub;
+    private LoaderManager.LoaderCallbacks<ArrayList<MarketIem>> btnSearch;
+
+    private ArrayList<String> valsMarket;
+    private ArrayList<StateAndSuburb> valsSub;
+    private ArrayList<MarketIem> marketItems;
+    private String query = "";
+    String querySub,subV,subType,q;
+    int stateid = 0;
+
     boolean isLoading = false;
     String name,image,synopsis,url1,city,city_id;
     int id;
@@ -84,6 +113,22 @@ public class MarketListing extends AppCompatActivity implements NavigationView.O
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         marketIems = new ArrayList<>();
+        marketItems = new ArrayList<>();
+        valsSub = new ArrayList<>();
+        querySub = subV = subType = q = "";
+
+        ImageView imageView = (ImageView)findViewById(R.id.imgSearch);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RequestAlertDialogBox();
+            }
+        });
+
+
+
+
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -150,6 +195,153 @@ public class MarketListing extends AppCompatActivity implements NavigationView.O
         requestQueue = Volley.newRequestQueue(this);
         parseJSON();
         initScrollListener();
+    }
+
+    private void RequestAlertDialogBox()
+    {
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//
+//        // get the layout inflater
+//        LayoutInflater inflater = this.getLayoutInflater();
+//
+//        // inflate and set the layout for the dialog
+//        // pass null as the parent view because its going in the dialog layout
+//        builder.setView(inflater.inflate(R.layout.search_alert_dialog_box, null))
+//                .show();
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.search_alert_dialog_box);
+        autoCompleteTextViewOne = dialog.findViewById(R.id.autoCompleteTextViewOne);
+        autoCompleteTextViewTwo = dialog.findViewById(R.id.autoCompleteTextViewTwo);
+        btnAlertDialogSearch = dialog.findViewById(R.id.btnAlertDialogSearch);
+
+
+        btnSearch = new LoaderManager.LoaderCallbacks<ArrayList<MarketIem>>() {
+            @Override
+            public Loader<ArrayList<MarketIem>> onCreateLoader(int id, Bundle args) {
+                LoaderBtnSrchMarket loaderBtnSearch = new LoaderBtnSrchMarket(MarketListing.this,q,subV,"http://serv.kesbokar.com.au/jil.0.1/v2/product",stateid,subType,0.0,0.0);
+                return loaderBtnSearch;
+            }
+
+            @Override
+            public void onLoadFinished(Loader<ArrayList<MarketIem>> loader, ArrayList<MarketIem> data) {
+                switch (loader.getId()){
+                    case LOADER_ID_BTNSRCH:
+                        if(data != null){
+                            marketItems = data;
+                            Log.i("Search", data.toString());
+                            Intent intent = new Intent(MarketListing.this,MarketListing.class);
+                            intent.putExtra("CHOICE", "btnSearch");
+                            intent.putParcelableArrayListExtra("ARRAYLIST",marketItems);
+                            startActivity(intent);
+                            //Toast.makeText(Navigation_market.this, data, Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                }
+            }
+
+            @Override
+            public void onLoaderReset(Loader<ArrayList<MarketIem>> loader) {
+
+            }
+        };
+        marketSub = new LoaderManager.LoaderCallbacks<ArrayList<StateAndSuburb>>() {
+            @Override
+            public Loader<ArrayList<StateAndSuburb>> onCreateLoader(int id, Bundle args) {
+                LoaderBusSuburb loaderBusSuburb = new LoaderBusSuburb(MarketListing.this,querySub,"http://serv.kesbokar.com.au/jil.0.1/v2/product/search/cities");
+                return loaderBusSuburb;
+            }
+
+            @Override
+            public void onLoadFinished(Loader<ArrayList<StateAndSuburb>> loader, ArrayList<StateAndSuburb> data) {
+                if (data.size() != 0) {
+                    valsSub = data;
+                    Log.i("Tag Sub", valsSub + "");
+                    ArrayAdapter<StateAndSuburb> adapter=new ArrayAdapter<StateAndSuburb>(MarketListing.this,android.R.layout.simple_dropdown_item_1line,valsSub);
+
+                    autoCompleteTextViewTwo.setAdapter(adapter);
+                } else {
+                    Toast.makeText(MarketListing.this, "No internet Connection", Toast.LENGTH_SHORT).show();
+                }
+                getLoaderManager().destroyLoader(LOADER_ID_MARVAL);
+            }
+
+            @Override
+            public void onLoaderReset(Loader<ArrayList<StateAndSuburb>> loader) {
+
+            }
+        };
+        marketSearch = new LoaderManager.LoaderCallbacks<ArrayList<String>>() {
+            @Override
+            public Loader<ArrayList<String>> onCreateLoader(int id, Bundle args) {
+                LoaderMarketSearch loaderMarketSearch= new LoaderMarketSearch(MarketListing.this,"","http://serv.kesbokar.com.au/jil.0.1/v2/product/search");
+                return loaderMarketSearch;
+            }
+
+            @Override
+            public void onLoadFinished(Loader<ArrayList<String>> loader, ArrayList<String> data) {
+                if (data.size() != 0) {
+                    valsMarket = data;
+                    Log.i("Tag", valsMarket + "");
+                    ArrayAdapter<String> adapter=new ArrayAdapter<String>(MarketListing.this,android.R.layout.simple_dropdown_item_1line,valsMarket);
+
+                    autoCompleteTextViewOne.setAdapter(adapter);
+                    getLoaderManager().initLoader(LOADER_ID_MARSUB,null,marketSub);
+                } else {
+                    Toast.makeText(MarketListing.this, "No internet Connection", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onLoaderReset(Loader<ArrayList<String>> loader) {
+
+            }
+        };
+
+        getLoaderManager().initLoader(LOADER_ID_MARVAL, null, marketSearch);
+        dialog.show();
+        btnAlertDialogSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                q = autoCompleteTextViewOne.getText().toString();
+                Log.i("Q and subV", q + " " + subV);
+                if(q.length() == 0 && subV.length() == 0){
+                    Toast.makeText(MarketListing.this, "Cannot Search Empty fields", Toast.LENGTH_SHORT).show();
+                }
+                else if (subV.length()==0)
+                {
+                    Toast.makeText(MarketListing.this, "Cannot Search Empty State", Toast.LENGTH_SHORT).show();
+                }
+                getLoaderManager().initLoader(LOADER_ID_BTNSRCH,null,btnSearch);
+            }
+        });
+
+        autoCompleteTextViewTwo.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                StateAndSuburb stateAndSuburb = (StateAndSuburb) parent.getAdapter().getItem(position);
+                subV = stateAndSuburb.getValue();
+                stateid = stateAndSuburb.getId();
+                subType = stateAndSuburb.getType();
+            }
+        });
+        autoCompleteTextViewTwo.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                querySub = s.toString();
+                getLoaderManager().restartLoader(LOADER_ID_MARSUB,null,marketSub);            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
 
     private void parseJSON() {
