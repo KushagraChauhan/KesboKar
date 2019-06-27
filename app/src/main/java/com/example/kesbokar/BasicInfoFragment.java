4b2a7a3aee40e2a80f69857d7290a83d335f7dbb
itsSiddharthGupta <36455409+itsSiddharthGupta@users.noreply.github.com>
2019-06-27 14:37:17 +0530

    Second Category calling done

--- app/src/main/java/com/example/kesbokar/BasicInfoFragment.java
+++ app/src/main/java/com/example/kesbokar/BasicInfoFragment.java
@@ -3,6 +3,8 @@ package com.example.kesbokar;
 
 import android.app.AlertDialog;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.loader.app.LoaderManager;
 import android.content.Context;
 
@@ -22,6 +24,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -40,7 +43,7 @@ public class BasicInfoFragment extends Fragment {
     String condition1, condition2;
     RadioGroup rgProductCondition, rgProductSelection;
     private Context context;
-    private int parent_id = 0;
+    private String parent_id = "";
     private static final int LOADER_FIRST_CATEGORY = 101;
     private static final int LOADER_SECOND_CATEGORY = 102;
     private static final int LOADER_THIRD_CATEGORY = 103;
@@ -49,6 +52,8 @@ public class BasicInfoFragment extends Fragment {
     private LoaderManager.LoaderCallbacks<ArrayList<CategorySecond>> secondCategoryLoader;
     private LoaderManager.LoaderCallbacks<ArrayList<CategoryThird>> thirdCategoryLoader;
 
+    private ListView listCategoriesBase;
+    private ArrayList<CategoryBase> categoryBaseArrayList;
 
     EditText edtProductTitle;
     public BasicInfoFragment() {
@@ -75,22 +80,27 @@ public class BasicInfoFragment extends Fragment {
         final String[] firstValueArray = {"API1", "API1", "API1"};
         final String[] secondValueArray;
         final String[] thirdValueArray;
+        categoryBaseArrayList = new ArrayList<>();
 
         btnCatFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                builder.setTitle("Select Option");
-                builder.setItems(firstValueArray, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int item) {
-                        value[0] = firstValueArray[item];
-                        btnCatFirst.setText(value[0]);
-                        btnCatSecond.setVisibility(View.VISIBLE);
+                getLoaderManager().restartLoader(LOADER_FIRST_CATEGORY, null, firstCategoryLoader);
+                Dialog dialog = new Dialog(getActivity());
+                View view1 = getActivity().getLayoutInflater().inflate(R.layout.category_base_dialog, null);
+                dialog.setContentView(view1);
+                listCategoriesBase = view1.findViewById(R.id.categoriesBaseListView);
+                CategoriesBaseAdapter categoriesBaseAdapter = new CategoriesBaseAdapter(getActivity(), categoryBaseArrayList);
+                listCategoriesBase.setAdapter(categoriesBaseAdapter);
+                listCategoriesBase.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        CategoryBase categoryBase = (CategoryBase) adapterView.getAdapter().getItem(i);
+                        parent_id = categoryBase.getId();
+                        Log.i("Parent id", parent_id);
                     }
                 });
-                AlertDialog alert = builder.create();
-                alert.show();
+                dialog.show();
             }
 
         });
@@ -109,6 +119,7 @@ public class BasicInfoFragment extends Fragment {
             @Override
             public void onLoadFinished(Loader<ArrayList<CategoryBase>> loader, ArrayList<CategoryBase> categoryBases) {
                 if(categoryBases!=null){
+                    categoryBaseArrayList = categoryBases;
                     Log.i("API RESULT Category", "onLoadFinished: " + categoryBases);
                 }
             }
@@ -118,8 +129,29 @@ public class BasicInfoFragment extends Fragment {
 
             }
         };
+        secondCategoryLoader = new LoaderManager.LoaderCallbacks<ArrayList<CategorySecond>>() {
+            @NonNull
+            @Override
+            public Loader<ArrayList<CategorySecond>> onCreateLoader(int id, @Nullable Bundle args) {
+                LoaderCategorySecond loaderCategorySecond = new LoaderCategorySecond(context, "http://serv.kesbokar.com.au/jil.0.1/v1/category?parent_id=" + parent_id + "&api_token=FSMNrrMCrXp2zbym9cun7phBi3n2gs924aYCMDEkFoz17XovFHhIcZZfCCdK");
+                return loaderCategorySecond;
+            }
 
+            @Override
+            public void onLoadFinished(@NonNull Loader<ArrayList<CategorySecond>> loader, ArrayList<CategorySecond> data) {
+                if(data!=null){
+                    Log.i("API SECOND", data.get(0).getTitle());
+                }
+            }
 
+            @Override
+            public void onLoaderReset(@NonNull Loader<ArrayList<CategorySecond>> loader) {
+
+            }
+        };
+
+        getLoaderManager().initLoader(LOADER_FIRST_CATEGORY, null, firstCategoryLoader);
+        getLoaderManager().initLoader(LOADER_SECOND_CATEGORY, null, secondCategoryLoader);
         btnCatSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -189,7 +221,7 @@ public class BasicInfoFragment extends Fragment {
         });
 
 
-        getLoaderManager().initLoader(LOADER_FIRST_CATEGORY, null, firstCategoryLoader);
+
 
 //        edtProductTitle = view.findViewById(R.id.etProductTitle);
 //        String data = "fail";
